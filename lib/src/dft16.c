30d866b2b77a681171377323045d752ace73552c
Mishaizlesa <misha11ru@gmail.com>
2024-02-06 17:37:30 +0300

    1st

--- lib/src/dft16.c
+++ lib/src/dft16.c
@@ -1,6 +1,61 @@
 #include "dft.h"
-
+#include "common_defs.h"
 extern void dft16Fwd(const cfloat32_t *pSrc, cfloat32_t *pDst)
 {
-    refDftFwd(pSrc, pDst, 16);
+    const float inv_sqrt2 = 0.7071067811865475f;
+    cfloat32_t tmpDst [16];
+    cfloat32_t tmpSrc[16];
+    uint32_t n1 = 4;
+    uint32_t n2 = 4;
+    uint32_t length = n1 * n2;
+//n1 = 4 n2 = 4
+    for (uint32_t i2 = 0; i2 < n2; i2++)
+    {
+        for (uint32_t i1 = 0; i1 < n1; i1++)
+        {
+            tmpSrc[i2 + i1 * n2].re = pSrc[n1 * i2 + i1].re;
+            tmpSrc[i2 + i1 * n2].im = pSrc[n1 * i2 + i1].im;
+        }
+    }
+     for (uint32_t i1 = 0; i1 < n1; i1++)
+    {
+        dft4Fwd(&tmpSrc[i1 * n2], &tmpDst[i1 * n2]);
+    }
+
+     for (uint32_t i1 = 1; i1 < n1; i1++)
+    {
+        for (uint32_t i2 = 0; i2 < n2; i2++)
+        {
+            float twdRe = cos(OWN_2PI * i2 * i1 / length);
+            float twdIm =-sin(OWN_2PI * i2 * i1 / length);
+            float re = tmpDst[i1 * n2 + i2].re * twdRe - tmpDst[i1 * n2 + i2].im * twdIm;
+            float im = tmpDst[i1 * n2 + i2].im * twdRe + tmpDst[i1 * n2 + i2].re * twdIm;
+            tmpDst[i1 * n2 + i2].re = re;
+            tmpDst[i1 * n2 + i2].im = im;
+        }
+    }
+
+    for (uint32_t i1 = 0; i1 < n1; i1++)
+    {
+        for (uint32_t i2 = 0; i2 < n2; i2++)
+        {
+            tmpSrc[n1 * i2 + i1].re = tmpDst[n2 * i1 + i2].re;
+            tmpSrc[n1 * i2 + i1].im = tmpDst[n2 * i1 + i2].im;
+        }
+    }
+
+     for (uint32_t i2 = 0; i2 < n2; i2++)
+    {
+        dft4Fwd(&tmpSrc[i2 * n1], &tmpDst[i2 * n1]);
+    }
+
+    for (uint32_t i2 = 0; i2 < n2; i2++)
+    {
+        for (uint32_t i1 = 0; i1 < n1; i1++)
+        {
+            pDst[n2 * i1 + i2].re = tmpDst[i1 + i2 * n1].re;
+            pDst[n2 * i1 + i2].im = tmpDst[i1 + i2 * n1].im;
+        }
+    }
+    return;
 }
--- lib/src/dft8.c
+++ lib/src/dft8.c
@@ -1,6 +1,90 @@
 #include "dft.h"
-
-extern void dft8Fwd(const cfloat32_t *pSrc, cfloat32_t *pDst)
+#include "math.h"
+#include <stdio.h>
+extern void dft8Fwd (const cfloat32_t *pSrc, cfloat32_t *pDst)
 {
-    refDftFwd(pSrc, pDst, 8);
+    const float inv_sqrt2 = 0.7071067811865475f;
+    cfloat32_t tmpDst [8];
+    
+    float tmp_op[8];
+    tmp_op[0] = pSrc[0].re  + pSrc[4].re;
+    tmp_op[1] = pSrc[2].re  + pSrc[6].re;
+    tmp_op[2] = pSrc[0].im   + pSrc[4].im;
+    tmp_op[3] = pSrc[2].im  + pSrc[6].im;
+    tmp_op[4] = pSrc[0].re   - pSrc[4].re;
+    tmp_op[5] = pSrc[2].im  - pSrc[6].im;
+    tmp_op[6] = pSrc[0].im   - pSrc[4].im;
+    tmp_op[7] = pSrc[2].re  - pSrc[6].re;
+
+    tmpDst[0].re   = tmp_op[0] + tmp_op[1];
+    tmpDst[0].im   = tmp_op[2] + tmp_op[3];
+    tmpDst[1].re   = tmp_op[4] + tmp_op[5];
+    tmpDst[1].im   = tmp_op[6] - tmp_op[7];
+    tmpDst[2].re   = tmp_op[0] - tmp_op[1];
+    tmpDst[2].im   = tmp_op[2] - tmp_op[3];
+    tmpDst[3].re   = tmp_op[4] - tmp_op[5];
+    tmpDst[3].im   = tmp_op[6] + tmp_op[7];  
+
+
+    tmp_op[0] = pSrc[1].re  + pSrc[5].re;
+    tmp_op[1] = pSrc[3].re  + pSrc[7].re;
+    tmp_op[2] = pSrc[1].im   + pSrc[5].im;
+    tmp_op[3] = pSrc[3].im  + pSrc[7].im;
+    tmp_op[4] = pSrc[1].re   - pSrc[5].re;
+    tmp_op[5] = pSrc[3].im  - pSrc[7].im;
+    tmp_op[6] = pSrc[1].im   - pSrc[5].im;
+    tmp_op[7] = pSrc[3].re  - pSrc[7].re ;
+
+
+    tmpDst[4].re   = tmp_op[0] + tmp_op[1];
+    tmpDst[4].im   = tmp_op[2] + tmp_op[3];
+    tmpDst[5].re   = tmp_op[4] + tmp_op[5];
+    tmpDst[5].im   = tmp_op[6] -tmp_op[7];
+    tmpDst[6].re   = tmp_op[0] -tmp_op[1];
+    tmpDst[6].im   = tmp_op[2] -tmp_op[3];
+    tmpDst[7].re   = tmp_op[4] -tmp_op[5];
+    tmpDst[7].im   = tmp_op[6] + tmp_op[7];  
+
+
+    float re = tmpDst[5].re * inv_sqrt2 + tmpDst[5].im * inv_sqrt2;
+    float im = tmpDst[5].im * inv_sqrt2 - tmpDst[5].re * inv_sqrt2;
+    tmpDst[5].re = re;
+    tmpDst[5].im = im;
+
+
+    im = -tmpDst[6].re;
+    tmpDst[6].re = tmpDst[6].im;
+    tmpDst[6].im = im;
+
+    re = -tmpDst[7].re * inv_sqrt2 + tmpDst[7].im * inv_sqrt2;
+    im = -tmpDst[7].im * inv_sqrt2 - tmpDst[7].re * inv_sqrt2;
+    tmpDst[7].re = re;
+    tmpDst[7].im = im;
+
+
+
+    pDst[4].re = tmpDst[0].re - tmpDst[4].re;
+    pDst[4].im = tmpDst[0].im - tmpDst[4].im;
+    pDst[0].re = tmpDst[0].re + tmpDst[4].re;
+    pDst[0].im = tmpDst[0].im + tmpDst[4].im;
+
+
+    pDst[5].re = tmpDst[1].re - tmpDst[5].re;
+    pDst[5].im = tmpDst[1].im - tmpDst[5].im;
+    pDst[1].re = tmpDst[1].re + tmpDst[5].re;
+    pDst[1].im = tmpDst[1].im + tmpDst[5].im;
+
+
+    pDst[6].re = tmpDst[2].re - tmpDst[6].re;
+    pDst[6].im = tmpDst[2].im - tmpDst[6].im;
+    pDst[2].re = tmpDst[2].re + tmpDst[6].re;
+    pDst[2].im = tmpDst[2].im + tmpDst[6].im;
+
+
+    pDst[3].re = tmpDst[3].re + tmpDst[7].re;
+    pDst[3].im = tmpDst[3].im + tmpDst[7].im;
+    pDst[7].re = tmpDst[3].re - tmpDst[7].re;
+    pDst[7].im = tmpDst[3].im - tmpDst[7].im;
+
+    return;
 }
